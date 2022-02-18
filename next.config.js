776ab39a0a6ecd4195acbe1783d1af0de0bb6a00
danny <danny@setel.com>
2022-02-18 17:52:53 +0000

    chore: fix image optimization loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    nextImageExportOptimizer: {
+      imageFolderPath: "public/images",
+      exportFolderPath: "out",
+      imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+      quality: 75,
+    },
+  },
+  env: {
+    storePicturesInWEBP: true,
+  },
 }
 
 module.exports = nextConfig
